93ee7a61f914dc19dc8de3ea186d98220b71c3f7
Dmytro Torop <young-z@i.ua>
2024-06-16 11:50:37 +0300

    feat: add shorten logic

--- src/homePage/HomePage.tsx
+++ src/homePage/HomePage.tsx
@@ -1,23 +1,27 @@
-import {HomePageView} from "./HomePageView";
-import {useState} from "react";
+import { HomePageView } from "./HomePageView";
+import { useState } from "react";
 import axios from "axios";
 
 export const HomePage = () => {
     const [link, setLink] = useState('');
+    const [isShortened, setIsShortened] = useState(false);
 
-    const handlePost = () => {
-        axios.post('https://cleanuri.com/api/v1/shorten', {
-            url: link
-        })
-            .then(response => {
-                console.log('Shortened URL:', response.data.result_url);
-            })
-            .catch(error => {
-                console.error('Error shortening URL:', error);
+
+        const handlePost = async () => {
+            const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+            const cleanUriApiUrl = 'https://cleanuri.com/api/v1/shorten';
+
+            try {
+            const response = await axios.post(corsAnywhereUrl + cleanUriApiUrl, {
+                url: link
             });
+            console.log('Shortened URL:', response.data.result_url);
+        } catch (error) {
+            console.error('Error shortening URL:', error);
+        }
     }
 
     return (
-            <HomePageView link={link} setLink={setLink} handlePost={handlePost}/>
-    )
+        <HomePageView link={link} setLink={setLink} handlePost={handlePost} />
+    );
 }
--- src/homePage/HomePageView.tsx
+++ src/homePage/HomePageView.tsx
@@ -4,6 +4,7 @@ import React from "react";
 interface Props {
     link: string;
     setLink: (text: string) => void;
+    handlePost: () => void;
 }
 export const HomePageView: React.FC<Props> = ({link, setLink, handlePost}) => (
     <>
@@ -11,7 +12,7 @@ export const HomePageView: React.FC<Props> = ({link, setLink, handlePost}) => (
             <h2 className="text-black text-3xl">Shortly!</h2>
             <p>Provide a link to shortify</p>
             <div className="flex gap-2">
-                <input type="text" className="rounded p-1" value={link} onChange={e => setLink(e.target.value)} />
+                <input type="text" className="rounded p-1 outline-none" value={link} onChange={e => setLink(e.target.value)} />
                 <button className="bg-amber-200 rounded p-1" onClick={handlePost}>Shortify</button>
                 <button className="bg-red-500 rounded p-1">Reset</button>
             </div>
